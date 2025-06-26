a25697d47ac19cdeb20275c0192d67bec88a98f4
viv-is-cool <viv42662@gmail.com>
2025-06-26 14:43:09 +0530

    Update playlist.py

--- playlist.py
+++ playlist.py
@@ -405,7 +405,7 @@ st.markdown("""
         border-radius: 10px;
         text-decoration: none;
         transition: all 0.3s ease;
-        margin-left: 800px;
+        margin-left: 500px;
             
     }
 
